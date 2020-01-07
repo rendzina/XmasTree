f9e8eab25c19f3fd6cd552a8222f8d7d01dc0881
rendzina <s.hallett@cranfield.ac.uk>
2020-01-07 12:01:45 +0000

    Update XmasTree_WhichLEDIsWhich.py

--- XmasTree_WhichLEDIsWhich.py
+++ XmasTree_WhichLEDIsWhich.py
@@ -9,16 +9,16 @@ from functools import partial
 
 # Instance the  RGBXmasTree
 tree = RGBXmasTree(brightness=0.05)
-tree.off()
+tree.off() # Start with all lights off
 
 def flashLED(i):
-    tree.off()
-    tree[i].color = (1, 1, 1)
-    print('LED ',i)
+    tree.off() # Turn all other LEDs off
+    tree[i].color = (1, 1, 1) # Light up respective LED
+    print('LED ',i) # Print out result
 
 master = Tk()
 canvas = Canvas(master)
-i=0
+i=0 # Note there are 25 LEDs on tree
 for c in range(5):
     for r in range(5):
         button = Button(0)
