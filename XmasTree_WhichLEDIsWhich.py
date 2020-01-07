509328c7e0582a9e650e5f3f322e3d702a10a0ef
rendzina <s.hallett@cranfield.ac.uk>
2020-01-07 02:29:35 +0000

    Create XmasTree_WhichLEDIsWhich.py

---
+++ XmasTree_WhichLEDIsWhich.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# XmasTree_WhichLEDIsWhich.py
+# Help identify which LED is which number - to aid development of patterns
+from tree import RGBXmasTree
+from tkinter import *
+from functools import partial
+
+# Instance the  RGBXmasTree
+tree = RGBXmasTree(brightness=0.05)
+tree.off()
+
+def flashLED(i):
+    tree.off()
+    tree[i].color = (1, 1, 1)
+    print('LED ',i)
+
+master = Tk()
+canvas = Canvas(master)
+i=0
+for c in range(5):
+    for r in range(5):
+        button = Button(0)
+        button.configure(text=i)
+        button.configure(command=partial(flashLED,i))
+        button.grid(row=r,column=c)
+        i+=1
+
+# main loop
+try:
+    master.mainloop()
+except KeyboardInterrupt:
+    tree.close()
